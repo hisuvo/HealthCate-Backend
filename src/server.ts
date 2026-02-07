83797269ede9bb62363755fd7280bb37995ca3ae
hisuvo <suvodatta72@gmail.com>
2026-02-07 23:44:01 +0600

    envVars create

--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,13 @@
 import app from "./app";
 import dotenv from "dotenv";
+import { envVars } from "./app/config/env";
 
 dotenv.config();
 
-const port = process.env.PORT;
-
 const bootstrap = async () => {
   try {
-    app.listen(port, () => {
-      console.log(`Server is running on http://localhost:${port}`);
+    app.listen(envVars.PORT, () => {
+      console.log(`Server is running on http://localhost:${envVars.PORT}`);
     });
   } catch (error) {
     console.error("Failed to start server", error);
